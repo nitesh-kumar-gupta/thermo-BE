38c0de643121023a2c70cea20339e26c83e34408
Nitesh Kumar Gupta <nitesh.gupta@venturepact.com>
2019-09-12 17:59:22 +0530

    allow origin

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,13 +3,13 @@ const controller = require('../../app/controllers');
 const constants = require('./../../config/constants');
 const response = require('./../response');
 module.exports = (app) => {
-    // app.use(function(req, res, next) {
-    //     res.header('Access-Control-Allow-Credentials', true);
-    //     res.header("Access-Control-Allow-Origin", "localhost");
-    //     res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
-    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    //     next();
-    // });
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header("Access-Control-Allow-Origin", "localhost");
+        res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    });
     app.get('/', controller.index);
     app.use('/api/v1', require('./v1')());
     app.use((req, res) => {
