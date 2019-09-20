d263cc0a3d0a95ffb5a20c71480de3bf9f614be9
Nitesh Kumar Gupta <nitesh.gupta@venturepact.com>
2019-09-20 13:01:29 +0530

    changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,7 @@ const response = require('./../response');
 module.exports = (app) => {
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Credentials', true);
-        res.header("Access-Control-Allow-Origin", "localhost");
+        res.header("Access-Control-Allow-Origin", "http://localhost:4200");
         res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         next();
